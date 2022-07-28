8a3a6d8bb71ba6137ca064eafb973123f29d1c4e
Luis Jimenez <eternal_pitta0b@icloud.com>
2022-07-28 18:01:50 -0500

    Add function resetGrid()

--- script.js
+++ script.js
@@ -25,3 +25,17 @@ function hoverFunction(e) {
 
     e.target.style.backgroundColor = `rgb(${vals[0]-decreaseBy},${vals[0]-decreaseBy},${vals[0]-decreaseBy})`
 }
+
+function resetGrid(e) {
+
+    if(!(e.target.id =='reset')) return;
+    container.replaceChildren();
+
+    let width = prompt("Enter width: (Width: 1-100)",1)
+
+    //hitting cancel, inputting 0, anything over 100, or a string will display 16x16 by default
+    if(width == null || width <= 0 || width > 100 || isNaN(width)){
+        return createGrid(16);
+    }    
+    createGrid(width);
+}
